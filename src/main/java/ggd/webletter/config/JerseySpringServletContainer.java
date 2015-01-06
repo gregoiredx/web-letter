af0fd50ad93a939b6899d4ec5be88b0103632681
Grégoire Deveaux <ggdx@laposte.net>
2015-01-06 23:27:35 +0100

    java 8 in JerseySpringServletContainer

--- src/main/java/ggd/webletter/config/JerseySpringServletContainer.java
+++ src/main/java/ggd/webletter/config/JerseySpringServletContainer.java
@@ -1,7 +1,6 @@
 package ggd.webletter.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.mvc.MvcFeature;
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
@@ -9,8 +8,9 @@ import org.springframework.beans.factory.ListableBeanFactory;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.ext.Provider;
-import java.lang.annotation.Annotation;
-import java.util.Map;
+
+import static org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature.TEMPLATES_BASE_PATH;
+import static org.glassfish.jersey.servlet.ServletProperties.FILTER_STATIC_CONTENT_REGEX;
 
 public class JerseySpringServletContainer extends ServletContainer {
 
@@ -20,19 +20,11 @@ public class JerseySpringServletContainer extends ServletContainer {
 
     public static class JerseyApplication extends ResourceConfig {
 
-        public JerseyApplication(ListableBeanFactory listableBeanFactory) {
-            registerBeansWithAnnotation(listableBeanFactory, Path.class);
-            registerBeansWithAnnotation(listableBeanFactory, Provider.class);
-            property(FreemarkerMvcFeature.TEMPLATES_BASE_PATH, "templates");
-            property(ServletProperties.FILTER_STATIC_CONTENT_REGEX, "/static");
-            register(FreemarkerMvcFeature.class);
-        }
-
-        private void registerBeansWithAnnotation(ListableBeanFactory listableBeanFactory, Class<? extends Annotation> anAnnotation) {
-            Map<String, Object> resources = listableBeanFactory.getBeansWithAnnotation(anAnnotation);
-            for (Map.Entry<String, Object> resource : resources.entrySet()) {
-                register(resource.getValue());
-            }
+        public JerseyApplication(ListableBeanFactory beanFactory) {
+            beanFactory.getBeansWithAnnotation(Path.class).values().forEach(this::register);
+            beanFactory.getBeansWithAnnotation(Provider.class).values().forEach(this::register);
+            register(FreemarkerMvcFeature.class).property(TEMPLATES_BASE_PATH, "templates");
+            property(FILTER_STATIC_CONTENT_REGEX, "/static");
         }
 
     }
